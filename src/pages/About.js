693b2b6229d24c05c2679e4a7b361a1d7a6e33eb
chandan9pandey <chandan9pandey@gmail.com>
2022-01-10 01:42:35 +0530

    initial-commit

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Vintage Tech is the global marketplace for unique and creative goods.
+				It’s home to a universe of special, extraordinary items, from unique
+				handcrafted pieces to vintage treasures.
+			</p>
+			<p>
+				In a time of increasing automation, it’s our mission to keep human
+				connection at the heart of commerce. That’s why we built a place where
+				creativity lives and thrives because it’s powered by people. We help our
+				community of sellers turn their ideas into successful businesses. Our
+				platform connects them with millions of buyers looking for an
+				alternative - something special with a human touch, for those moments in
+				life that deserve imagination.
+			</p>
+			<p>
+				As a company, we strive to lead with our guiding principles and to help
+				spread ideas of sustainability and responsibility whose impact can reach
+				far beyond our own business.
+			</p>
+		</section>
+	);
+}
